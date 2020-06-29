7a92018e5a020c935a2c75adfe078d043893d523
Sergio <sergiodepolo@hotmail.com>
2020-06-29 10:23:16 +0200

    Add comments

--- main/esp32_nat_router.c
+++ main/esp32_nat_router.c
@@ -197,7 +197,7 @@ void wifi_init(const char* ssid, const char* passwd, const char* ap_ssid, const 
     IP4_ADDR(&ipInfo.ip, 192,168,0,1);
 	IP4_ADDR(&ipInfo.gw, 192,168,0,1);
 	IP4_ADDR(&ipInfo.netmask, 255,255,254,0);
-    esp_netif_dhcps_stop(wifiAP);
+    esp_netif_dhcps_stop(wifiAP); // stop before setting ip WifiAP
 	esp_netif_set_ip_info(wifiAP, &ipInfo);
 	esp_netif_dhcps_start(wifiAP);
 
