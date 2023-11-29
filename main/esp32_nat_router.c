1b36d2a41f6ee7f07c9f06da4c60aa42d182d82b
Jacob Pedersen <jacobh_pedersen@hotmail.com>
2023-11-29 20:48:38 +0100

    POC usb serial support

--- main/esp32_nat_router.c
+++ main/esp32_nat_router.c
@@ -14,7 +14,9 @@
 #include "esp_log.h"
 #include "esp_console.h"
 #include "esp_vfs_dev.h"
+#include "esp_vfs_cdcacm.h"
 #include "driver/uart.h"
+#include "driver/usb_serial_jtag.h"
 #include "linenoise/linenoise.h"
 #include "argtable3/argtable3.h"
 #include "esp_vfs_fat.h"
@@ -234,70 +236,70 @@ esp_err_t del_portmap(u8_t proto, u16_t mport) {
     return ESP_OK;
 }
 
-static void initialize_console(void)
-{
-    /* Drain stdout before reconfiguring it */
-    fflush(stdout);
-    fsync(fileno(stdout));
+// static void initialize_console(void)
+// {
+//     /* Drain stdout before reconfiguring it */
+//     fflush(stdout);
+//     fsync(fileno(stdout));
 
-    /* Disable buffering on stdin */
-    setvbuf(stdin, NULL, _IONBF, 0);
+//     /* Disable buffering on stdin */
+//     setvbuf(stdin, NULL, _IONBF, 0);
 
-    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-    esp_vfs_dev_uart_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
-    /* Move the caret to the beginning of the next line on '\n' */
-    esp_vfs_dev_uart_port_set_tx_line_endings(0, ESP_LINE_ENDINGS_CRLF);
+//     /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+//     esp_vfs_dev_uart_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
+//     /* Move the caret to the beginning of the next line on '\n' */
+//     esp_vfs_dev_uart_port_set_tx_line_endings(0, ESP_LINE_ENDINGS_CRLF);
 
-    /* Configure UART. Note that REF_TICK is used so that the baud rate remains
-     * correct while APB frequency is changing in light sleep mode.
-     */
-    const uart_config_t uart_config = {
-            .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
-            .data_bits = UART_DATA_8_BITS,
-            .parity = UART_PARITY_DISABLE,
-            .stop_bits = UART_STOP_BITS_1,
-            #if defined(CONFIG_IDF_TARGET_ESP32) || defined(CONFIG_IDF_TARGET_ESP32S2)
-                .source_clk = UART_SCLK_REF_TICK,
-            #else
-                .source_clk = UART_SCLK_XTAL,
-            #endif
-    };
-    /* Install UART driver for interrupt-driven reads and writes */
-    ESP_ERROR_CHECK( uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM,
-            256, 0, 0, NULL, 0) );
-    ESP_ERROR_CHECK( uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config) );
+//     /* Configure UART. Note that REF_TICK is used so that the baud rate remains
+//      * correct while APB frequency is changing in light sleep mode.
+//      */
+//     const uart_config_t uart_config = {
+//             .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+//             .data_bits = UART_DATA_8_BITS,
+//             .parity = UART_PARITY_DISABLE,
+//             .stop_bits = UART_STOP_BITS_1,
+//             #if defined(CONFIG_IDF_TARGET_ESP32) || defined(CONFIG_IDF_TARGET_ESP32S2)
+//                 .source_clk = UART_SCLK_REF_TICK,
+//             #else
+//                 .source_clk = UART_SCLK_XTAL,
+//             #endif
+//     };
+//     /* Install UART driver for interrupt-driven reads and writes */
+//     ESP_ERROR_CHECK( uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM,
+//             256, 0, 0, NULL, 0) );
+//     ESP_ERROR_CHECK( uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config) );
 
-    /* Tell VFS to use UART driver */
-    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+//     /* Tell VFS to use UART driver */
+//     esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
 
-    /* Initialize the console */
-    esp_console_config_t console_config = {
-            .max_cmdline_args = 8,
-            .max_cmdline_length = 256,
-#if CONFIG_LOG_COLORS
-            .hint_color = atoi(LOG_COLOR_CYAN)
-#endif
-    };
-    ESP_ERROR_CHECK( esp_console_init(&console_config) );
+//     /* Initialize the console */
+//     esp_console_config_t console_config = {
+//             .max_cmdline_args = 8,
+//             .max_cmdline_length = 256,
+// #if CONFIG_LOG_COLORS
+//             .hint_color = atoi(LOG_COLOR_CYAN)
+// #endif
+//     };
+//     ESP_ERROR_CHECK( esp_console_init(&console_config) );
 
-    /* Configure linenoise line completion library */
-    /* Enable multiline editing. If not set, long commands will scroll within
-     * single line.
-     */
-    linenoiseSetMultiLine(1);
+//     /* Configure linenoise line completion library */
+//     /* Enable multiline editing. If not set, long commands will scroll within
+//      * single line.
+//      */
+//     linenoiseSetMultiLine(1);
 
-    /* Tell linenoise where to get command completions and hints */
-    linenoiseSetCompletionCallback(&esp_console_get_completion);
-    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+//     /* Tell linenoise where to get command completions and hints */
+//     linenoiseSetCompletionCallback(&esp_console_get_completion);
+//     linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
 
-    /* Set command history size */
-    linenoiseHistorySetMaxLen(100);
+//     /* Set command history size */
+//     linenoiseHistorySetMaxLen(100);
 
-#if CONFIG_STORE_HISTORY
-    /* Load command history from filesystem */
-    linenoiseHistoryLoad(HISTORY_PATH);
-#endif
-}
+// #if CONFIG_STORE_HISTORY
+//     /* Load command history from filesystem */
+//     linenoiseHistoryLoad(HISTORY_PATH);
+// #endif
+// }
 
 void * led_status_thread(void * p)
 {
@@ -508,9 +510,59 @@ char* param_set_default(const char* def_val) {
     return retval;
 }
 
+
+
+static void initialize_console_usb(void)
+{
+    /* Disable buffering on stdin */
+    setvbuf(stdin, NULL, _IONBF, 0);
+
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_cdcacm_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_cdcacm_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+
+    /* Enable non-blocking mode on stdin and stdout */
+    fcntl(fileno(stdout), F_SETFL, 0);
+    fcntl(fileno(stdin), F_SETFL, 0);
+
+    /* Initialize the console */
+    esp_console_config_t console_config = {
+            .max_cmdline_args = 8,
+            .max_cmdline_length = 256,
+#if CONFIG_LOG_COLORS
+            .hint_color = atoi(LOG_COLOR_CYAN)
+#endif
+    };
+    ESP_ERROR_CHECK( esp_console_init(&console_config) );
+
+    /* Configure linenoise line completion library */
+    /* Enable multiline editing. If not set, long commands will scroll within
+     * single line.
+     */
+    linenoiseSetMultiLine(1);
+
+    /* Tell linenoise where to get command completions and hints */
+    linenoiseSetCompletionCallback(&esp_console_get_completion);
+    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+
+    /* Set command history size */
+    linenoiseHistorySetMaxLen(10);
+}
+
+
 void app_main(void)
 {
     initialize_nvs();
+
+    
+    esp_console_repl_t *repl = NULL;
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    /* Prompt to be printed before each line.
+     * This can be customized, made dynamic, etc.
+     */
+    repl_config.prompt = ">";
+    repl_config.max_cmdline_length = 0;
 
 #if CONFIG_STORE_HISTORY
     initialize_filesystem();
@@ -582,7 +634,7 @@ void app_main(void)
     }
     free(lock);
 
-    initialize_console();
+    //initialize_console_usb();
 
     /* Register commands */
     esp_console_register_help_command();
@@ -590,68 +642,91 @@ void app_main(void)
     register_nvs();
     register_router();
 
+
+#if defined(CONFIG_ESP_CONSOLE_UART_DEFAULT) || defined(CONFIG_ESP_CONSOLE_UART_CUSTOM)
+    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+
+#elif defined(CONFIG_ESP_CONSOLE_USB_CDC)
+    esp_console_dev_usb_cdc_config_t hw_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&hw_config, &repl_config, &repl));
+
+#elif defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+    esp_console_dev_usb_serial_jtag_config_t hw_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl));
+
+#else
+#error Unsupported console type
+#endif
+
+    ESP_ERROR_CHECK(esp_console_start_repl(repl));
+
+
     /* Prompt to be printed before each line.
      * This can be customized, made dynamic, etc.
      */
-    const char* prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
+//     const char* prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
 
-    printf("\n"
-           "ESP32 NAT ROUTER\n"
-           "Type 'help' to get the list of commands.\n"
-           "Use UP/DOWN arrows to navigate through command history.\n"
-           "Press TAB when typing command name to auto-complete.\n");
+//     printf("\n"
+//            "ESP32 NAT ROUTER\n"
+//            "Type 'help' to get the list of commands.\n"
+//            "Use UP/DOWN arrows to navigate through command history.\n"
+//            "Press TAB when typing command name to auto-complete.\n");
 
-    if (strlen(ssid) == 0) {
-         printf("\n"
-               "Unconfigured WiFi\n"
-               "Configure using 'set_sta' and 'set_ap' and restart.\n");       
-    }
+//     if (strlen(ssid) == 0) {
+//          printf("\n"
+//                "Unconfigured WiFi\n"
+//                "Configure using 'set_sta' and 'set_ap' and restart.\n");       
+//     }
 
-    /* Figure out if the terminal supports escape sequences */
-    int probe_status = linenoiseProbe();
-    if (probe_status) { /* zero indicates success */
-        printf("\n"
-               "Your terminal application does not support escape sequences.\n"
-               "Line editing and history features are disabled.\n"
-               "On Windows, try using Putty instead.\n");
-        linenoiseSetDumbMode(1);
-#if CONFIG_LOG_COLORS
-        /* Since the terminal doesn't support escape sequences,
-         * don't use color codes in the prompt.
-         */
-        prompt = "esp32> ";
-#endif //CONFIG_LOG_COLORS
-    }
+//     /* Figure out if the terminal supports escape sequences */
+//     int probe_status = linenoiseProbe();
+//     if (probe_status) { /* zero indicates success */
+//         printf("\n"
+//                "Your terminal application does not support escape sequences.\n"
+//                "Line editing and history features are disabled.\n"
+//                "On Windows, try using Putty instead.\n");
+//         linenoiseSetDumbMode(1);
+// #if CONFIG_LOG_COLORS
+//         /* Since the terminal doesn't support escape sequences,
+//          * don't use color codes in the prompt.
+//          */
+//         prompt = "esp32> ";
+// #endif //CONFIG_LOG_COLORS
+//     }
 
-    /* Main loop */
-    while(true) {
-        /* Get a line using linenoise.
-         * The line is returned when ENTER is pressed.
-         */
-        char* line = linenoise(prompt);
-        if (line == NULL) { /* Ignore empty lines */
-            continue;
-        }
-        /* Add the command to the history */
-        linenoiseHistoryAdd(line);
-#if CONFIG_STORE_HISTORY
-        /* Save command history to filesystem */
-        linenoiseHistorySave(HISTORY_PATH);
-#endif
+//     /* Main loop */
+//     while(true) {
+//         /* Get a line using linenoise.
+//          * The line is returned when ENTER is pressed.
+//          */
+//         char* line = linenoise(prompt);
+//         if (line == NULL) { /* Ignore empty lines */
+//             continue;
+//         }
+//         /* Add the command to the history */
+//         linenoiseHistoryAdd(line);
+// #if CONFIG_STORE_HISTORY
+//         /* Save command history to filesystem */
+//         linenoiseHistorySave(HISTORY_PATH);
+// #endif
 
-        /* Try to run the command */
-        int ret;
-        esp_err_t err = esp_console_run(line, &ret);
-        if (err == ESP_ERR_NOT_FOUND) {
-            printf("Unrecognized command\n");
-        } else if (err == ESP_ERR_INVALID_ARG) {
-            // command was empty
-        } else if (err == ESP_OK && ret != ESP_OK) {
-            printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
-        } else if (err != ESP_OK) {
-            printf("Internal error: %s\n", esp_err_to_name(err));
-        }
-        /* linenoise allocates line buffer on the heap, so need to free it */
-        linenoiseFree(line);
-    }
+//         /* Try to run the command */
+//         int ret;
+//         esp_err_t err = esp_console_run(line, &ret);
+//         if (err == ESP_ERR_NOT_FOUND) {
+//             printf("Unrecognized command\n");
+//         } else if (err == ESP_ERR_INVALID_ARG) {
+//             // command was empty
+//         } else if (err == ESP_OK && ret != ESP_OK) {
+//             printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
+//         } else if (err != ESP_OK) {
+//             printf("Internal error: %s\n", esp_err_to_name(err));
+//         }
+//         /* linenoise allocates line buffer on the heap, so need to free it */
+//         linenoiseFree(line);
+//     }
+
+//     ESP_LOGE(TAG, "Error or end-of-input, terminating console");
+//     esp_console_deinit();
 }
